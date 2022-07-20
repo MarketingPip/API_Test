902edc00634def72dc8c7ea698c5a76ed3c18c31
MarketingPip <86180097+MarketingPip@users.noreply.github.com>
2022-07-20 14:35:32 -0400

    Update test.js

--- api/test.js
+++ api/test.js
@@ -85,6 +85,10 @@ if (url == "name"){
 
   findMatchResults('name', Route)
  
+    var Message =  ` Blog posts with name's found containing:  ${c}  
+         <br>
+  
+         ${blogPosts} `
   
   
 }
@@ -92,6 +96,13 @@ if (url == "name"){
 if (url == "url"){
   
   findMatchResults('url', Route)
+    
+    var Message =  ` Blog posts with URL's found containing:  ${c}  
+         <br>
+  
+         ${blogPosts} `
+   
+    
 }       
        
        
@@ -111,10 +122,7 @@ if (url == "url"){
      } 
        if (SearchResults === true) {
          
-         document.body.innerHTML = ` Blog posts found containing:  ${c}  
-         <br>
-  
-         ${blogPosts} `
+         document.body.innerHTML = Message
          
        } else {
           document.body.innerHTML = Message
